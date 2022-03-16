c4669855bfe374c0638b2defb4f64082965c6c3a
abcpaem <abcpaem@gmail.com>
2022-03-16 14:41:50 +0000

    Fixes checkGetIceCreamCodeForMintChocolateChip test from Exercise003

--- src/main/java/com/techreturners/exercise003/Exercise003.java
+++ src/main/java/com/techreturners/exercise003/Exercise003.java
@@ -1,9 +1,13 @@
 package com.techreturners.exercise003;
 
+import java.util.Arrays;
+
 public class Exercise003 {
 
+    private String[] iceCreamFlavours = { "Pistachio", "Raspberry Ripple", "Vanilla", "Mint Chocolate Chip", "Chocolate", "Mango Sorbet" };
+
     int getIceCreamCode(String iceCreamFlavour) {
-        throw new UnsupportedOperationException(("You can delete this statement and add your code here."));
+        return Arrays.asList(iceCreamFlavours).indexOf(iceCreamFlavour);
     }
 
     String[] iceCreamFlavours() {
