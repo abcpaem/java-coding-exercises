40686034f608818d495cec03ebd3941b2866dfef
abcpaem <abcpaem@gmail.com>
2022-03-18 00:00:33 +0000

    Fixes the magic number issue in Exercise004

--- src/main/java/com/techreturners/exercise004/Exercise004.java
+++ src/main/java/com/techreturners/exercise004/Exercise004.java
@@ -4,19 +4,19 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 public class Exercise004 {
+    public static final long GIGASECOND = 1000000000;
     private LocalDateTime dateTime;
 
     public Exercise004(LocalDate date) {
-        // Test is expecting the class to add 1000000000 seconds to the parameter sent to constructor
-        this.dateTime = date.atStartOfDay().plusSeconds(1000000000);
+        this.dateTime = date.atStartOfDay();
     }
 
     public Exercise004(LocalDateTime dateTime) {
-        // Test is expecting the class to add 1000000000 seconds to the parameter sent to constructor
-        this.dateTime = dateTime.plusSeconds(1000000000);
+        this.dateTime = dateTime;
     }
 
+    // Given a moment in time set in constructor, this method returns the time after a gigasecond has passed
     public LocalDateTime getDateTime() {
-        return dateTime;
+        return dateTime.plusSeconds(GIGASECOND);
     }
 }
